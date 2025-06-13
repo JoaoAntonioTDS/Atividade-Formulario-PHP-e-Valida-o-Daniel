e919013bd9fcd9273ecdf3b006d9e766b53e3040
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2025-06-13 15:30:07 -0300

    Update banco.sql

--- carros/banco.sql
+++ carros/banco.sql
@@ -2,8 +2,8 @@ CREATE TABLE carros (
     id INTEGER NOT NULL AUTO_INCREMENT,
     marca VARCHAR(20) NOT NULL,
     modelo VARCHAR(20) NOT NULL,
-    motor VARCHAR(1) NOT NULL, -- E = Elétrico, C = Combustão, H = Híbrido
-    cor VARCHAR(1) NOT NULL,   -- B, P, V, A, Z, E, C, R, M (códigos das cores)
+    motor VARCHAR(1) NOT NULL, 
+    cor VARCHAR(1) NOT NULL,   
     ano INTEGER NOT NULL,
     CONSTRAINT pk_carros PRIMARY KEY (id)
 );
